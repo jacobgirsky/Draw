596df575f12a7e7bb5eb30703458da68c1d69f26
jacobgirsky <jgirsky@my.apsu.edu>
2019-03-21 09:20:20 -0500

    Added button functionality

--- app/src/main/java/edu/apsu/draw/MainActivity.java
+++ app/src/main/java/edu/apsu/draw/MainActivity.java
@@ -1,13 +1,62 @@
 package edu.apsu.draw;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    private static final int PICK_IMAGE = 100;
+    private static final int CAMERA_IMAGE = 101;
+    Uri imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageview);
+
+        // onClickListener for the choose photo button
+        findViewById(R.id.choose_photo_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, PICK_IMAGE);
+            }
+        });
+
+
+        findViewById(R.id.take_photo_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_IMAGE);
+            }
+        });
     }
-}
+
+        // depending on which button the user presses either the camera will load or the gallery will load
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+                imageURI = data.getData();
+                imageView.setImageURI(imageURI);
+            } else if (resultCode == RESULT_OK && requestCode == CAMERA_IMAGE) {
+                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+
+    }
+
