21e81e868ed8cf87c75aec1388380ebc602b069a
jacobgirsky <jgirsky@my.apsu.edu>
2019-03-21 09:38:07 -0500

    Added button functionality

--- app/src/main/java/edu/apsu/draw/MainActivity.java
+++ app/src/main/java/edu/apsu/draw/MainActivity.java
@@ -2,6 +2,9 @@ package edu.apsu.draw;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.net.URI;
 
@@ -26,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageview);
 
+
         // onClickListener for the choose photo button
         findViewById(R.id.choose_photo_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                startActivityForResult(gallery, PICK_IMAGE);
+                openGallery();
             }
         });
 
@@ -45,18 +49,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-        // depending on which button the user presses either the camera will load or the gallery will load
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
-                imageURI = data.getData();
-                imageView.setImageURI(imageURI);
-            } else if (resultCode == RESULT_OK && requestCode == CAMERA_IMAGE) {
-                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-                imageView.setImageBitmap(bitmap);
-            }
-        }
-
+    // opens the gallery and allows the user to select a photo
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+        TextView tv = findViewById(R.id.select_photo_tv);
+        tv.setText("");
     }
+
+
+    // depending on which button the user presses either the camera will load or the gallery will load
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageURI = data.getData();
+            imageView.setImageURI(imageURI);
+        } else if (resultCode == RESULT_OK && requestCode == CAMERA_IMAGE) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+
+}
 
